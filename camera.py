9ab86ef58d902aa287f9df83c9a0c66bfe01f2b1
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-04 18:57:45 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -6,10 +6,8 @@ print("press esc button to turn off the camera")
 while True:
     img=cam.read()[1]
     cv2.imshow("Cam feed",img)
-    k=cv2.waitKey(1) & 0xFF
-    if k==27:
+    k=cv2.waitKey(1) & 0xFF     # until u press esc button camera will be shown
+    if k==27:               # 27 represents the keyboard value of esc button if u want to change it,u can change
         break    
 cam.release()
 cv2.destroyAllWindows()
-
-
